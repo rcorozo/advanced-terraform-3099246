77496a66cacca3be82b09620dfd2208e468adcd1
Rodrigo Corozo <102171121+rcorozo@users.noreply.github.com>
2023-01-26 13:31:41 -0500

    Update ouputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "ephemeral_public_ip" {
-    value = google_compute_instance.nginx_instance.network_interface.0.network_ip
+    value = google_compute_instance.nginx_instance.network_interface.0.access_config.0.nat_ip 
 }
